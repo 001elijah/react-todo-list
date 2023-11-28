56142bc1bab652a5f57db60278cce3e38d38863b
illia_zolotukha <job.illia.igorevich@gmail.com>
2023-11-28 12:01:03 +0200

    ADD: toggle completed, delete task; UPDATE: tasks remaining display

--- src/App.js
+++ src/App.js
@@ -12,9 +12,38 @@ function App({ taskItems }) {
     setTasks([...tasks, newTask]);
   };
 
+  const toggleTaskCompleted = (id) => {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  const deleteTask = (id) => {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
   const taskList = tasks.map(({ id, name, completed }) => (
-    <Todo id={id} name={name} completed={completed} key={id} />
+    <Todo
+      id={id}
+      name={name}
+      completed={completed}
+      key={id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
   ));
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -24,7 +53,7 @@ function App({ taskItems }) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
