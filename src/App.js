1d4cd198fcc1d7120d09a3ecb16ec1d10631897e
illia_zolotukha <job.illia.igorevich@gmail.com>
2023-11-28 13:57:07 +0200

    UPDATE: filter

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import FilterButton from "./components/FilterButton/FilterButton";
 import Form from "./components/Form/Form";
 import Todo from "./components/Todo/Todo";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App({ taskItems }) {
   const [tasks, setTasks] = useState(taskItems);
+  const [filter, setFilter] = useState("All");
 
   const addTask = (name) => {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
@@ -35,22 +44,33 @@ function App({ taskItems }) {
       return task;
     });
     setTasks(editedTaskList);
-  }
+  };
 
   const deleteTask = (id) => {
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
   };
 
-  const taskList = tasks.map(({ id, name, completed }) => (
-    <Todo
-      id={id}
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
       name={name}
-      completed={completed}
-      key={id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
 
@@ -61,11 +81,7 @@ function App({ taskItems }) {
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         className="todo-list stack-large stack-exception"
--- src/components/FilterButton/FilterButton.js
+++ src/components/FilterButton/FilterButton.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-const FilterButton = (props) => {
+const FilterButton = ({ name, isPressed, setFilter }) => {
   return (
-    <button type="button" className="btn toggle-btn" aria-pressed="true">
+    <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={isPressed}
+      onClick={() => setFilter(name)}
+    >
       <span className="visually-hidden">Show </span>
-      <span>all </span>
+      <span>{name}</span>
       <span className="visually-hidden"> tasks</span>
     </button>
   );
-}
+};
 
 export default FilterButton;
